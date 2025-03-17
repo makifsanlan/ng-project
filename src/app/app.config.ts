4873e37fe4a85e2e884585fa4d3bb4760b04f164
makifsanlan <your-email@example.com>
2025-03-17 03:47:45 +0300

    authorization iişlemleri ve aspect kullanımı

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,29 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), RouterModule,provideAnimations(),
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+    RouterModule,
+    provideAnimations(),
     provideToastr({
       positionClass: 'toast-bottom-right'
-    })]
+    }),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useValue: AuthInterceptor,
+      multi: true  // Birden fazla interceptor eklenebilmesi için
+    }
+  ]
 };
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,37 +3,45 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-loginForm: FormGroup;
-constructor( private formBuilder: FormBuilder, private authService:AuthService) {  }
+  loginForm:FormGroup;
+  constructor(private formBuilder:FormBuilder,
+     private authService:AuthService, private toastrService:ToastrService) { }
 
-ngOnInit(): void {
-  this.createLoginForm();
-}
+  ngOnInit(): void {
+    this.createLoginForm();
+  }
 
-createLoginForm() {
- this.loginForm = this.formBuilder.group({
-   email: ['', Validators.required],
-    password: ['', Validators.required]
-  });
-}
-
-login(){
-  if(this.loginForm.valid){
-    console.log(this.loginForm.value);
-    let loginModel = Object.assign({}, this.loginForm.value)
-    this.authService.login(loginModel).subscribe(response=>{
-      console.log(response);
+  createLoginForm(){
+    this.loginForm = this.formBuilder.group({
+      email: ["",Validators.required],
+      password:["",Validators.required]
     })
   }
-}
+
+  login(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+      let loginModel = Object.assign({},this.loginForm.value)
+
+      this.authService.login(loginModel).subscribe(response=>{
+        this.toastrService.info(response.message)
+        localStorage.setItem("token",response.data.token)
+      },responseError=>{
+        //console.log(responseError)
+        this.toastrService.error(responseError.error)
+      })
+    }
+  }
 
 }
